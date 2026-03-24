4c16fac34a80b5012be32eb5e72061c70974eb1e
21ECM01_EstuardoChavarría <e.chavarria.5bdb@gmail.com>
2026-03-24 09:52:45 -0600

    Add files via upload
    Ejercicio en clase

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.Design;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //un algoritmo que determine si un estudiante ganó o perdió segun el promedio
+
+        Console.WriteLine("Hello, World!");
+        //Variables
+        string materia;
+        int n1, n2, n3, n4, n5;
+        double promedio;
+
+        //enviar info al asuario
+        Console.WriteLine("Digite una Materia: ");
+        materia = Console.ReadLine();
+        Console.WriteLine("Ingrese sus notas: ");
+        n1=Convert.ToInt32(materia);    
+        n2=Convert.ToInt32(materia);
+        n3=Convert.ToInt32(materia);
+        n4=Convert.ToInt32(materia);
+        n5=Convert.ToInt32(materia);
+
+        //Proceso
+        promedio = (n1 + n2 + n3 + n4 + n5) / 5;
+
+        //Evaluar si gano o perdio
+        if (promedio < 70)
+           Console.WriteLine("Estudiante reprobado con: "+promedio);
+        else
+            Console.WriteLine("Estudiante reprobado con:" + promedio);
+    }
+}
