54b468aa2bb5da50ca3d084c370a749f88db3557
21ECM01_EstuardoChavarría <e.chavarria.5bdb@gmail.com>
2026-09-22 15:39:02 -0600

    Add files via upload

---
+++ Problema5_EstuardoChavarríaIVC/Problema5_EstuardoChavarríaIVC/Form1.cs
@@ -0,0 +1,10 @@
+namespace Problema5_EstuardoChavarríaIVC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
